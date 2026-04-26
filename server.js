6e9166113e93b6c313f300af284c7012b50328d8
kilid999 <kilid99fr@gmail.com>
2026-04-26 23:44:03 +0300

    server.js

--- server.js
+++ server.js
@@ -57,6 +57,8 @@ app.post('/detect', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('API running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
